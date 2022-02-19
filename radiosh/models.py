5dd119d1dc028a92f1fca82101ee43e0b3285a28
Osama Ammar <ossssamar@gmail.com>
2022-02-18 22:41:27 -0800

    making the server opens at specific html file

--- radiosh/models.py
+++ radiosh/models.py
@@ -2,7 +2,7 @@ from django.conf import settings
 from django.db import models
 from django.utils import timezone
 
-
+#making a model
 class Post(models.Model):
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
